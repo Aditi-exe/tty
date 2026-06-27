5bb436ef9a1ca97da7f322c53148c3b08282a4c0
Aditi Sathe <aditisathe97@gmail.com>
2026-06-27 18:00:17 +0530

    feature: input scroll improvements; remove output scroll (to be implemented through mouse later)

--- src/app.rs
+++ src/app.rs
@@ -7,6 +7,9 @@ pub struct App
     pub input: String,
     pub history: Vec<(String, String)>,
     pub output: Vec<String>,
+    // TODO: add scroll: u16 and visible_height: usize for mouse scroll on output
+    pub history_index: Option<usize>,
+    pub saved_input: String,
 }
 
 impl App
@@ -18,6 +21,8 @@ impl App
             input: String::new(),
             history: load_history(),
             output: Vec::new(),
+            history_index: None,
+            saved_input: String::new(),
         }
     }
 
@@ -48,5 +53,38 @@ impl App
             save_line(&timestamp, &input);
         }
         false
+    }
+
+    pub fn history_up(&mut self) {
+        if self.history.is_empty()
+        {
+            return;
+        }
+        let new_index = match self.history_index
+        {
+            None => 
+            {
+                self.saved_input = self.input.clone();
+                self.history.len() - 1
+            }
+            Some(i) => i.saturating_sub(1),
+        };
+        self.history_index = Some(new_index);
+        self.input = self.history[new_index].1.clone();
+    }
+
+    pub fn history_down(&mut self) {
+        match self.history_index {
+            None => {}
+            Some(i) => {
+                if i + 1 >= self.history.len() {
+                    self.history_index = None;
+                    self.input = self.saved_input.clone();
+                } else {
+                    self.history_index = Some(i + 1);
+                    self.input = self.history[i + 1].1.clone();
+                }
+            }
+        }
     }
 }
